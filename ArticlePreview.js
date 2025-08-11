4f0dfcc74a93c21ac8e5a3b8fe5c6b7104bbb1d4
Ivan solano <ivanheerr13@gmail.com>
2025-08-11 16:54:23 -0500

    Add: diseño principal

--- ArticlePreview.js
+++ ArticlePreview.js
@@ -1,13 +1,13 @@
 class ArticlePreview extends HTMLElement {
     static get observedAttributes(){
-        return ['autor', 'title', 'perfil', 'portada']
+        return ['autor', 'titulo', 'perfil', 'portada']
     };
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
         this._datos = {};
         this._autor = '';
-        this._title = '';
+        this._titulo = '';
         this._perfil = '';
         this._portada = '';
     }
@@ -18,9 +18,9 @@ class ArticlePreview extends HTMLElement {
                 this._autor = newValue;
                 this._datos.autor = newValue;
                 break;
-            case 'title':
-                this._title = newValue;
-                this._datos.title = newValue;
+            case 'titulo':
+                this._titulo = newValue;
+                this._datos.titulo = newValue;
                 break;
             case 'perfil':
                 this._perfil = newValue;
@@ -38,14 +38,15 @@ class ArticlePreview extends HTMLElement {
         this.render();
     }
     
+
     set datos(opciones) {
         this._autor = opciones.autor || this._autor;
-        this._title = opciones.title || this._title;
+        this._titulo = opciones.titulo || this._titulo;
         this._perfil = opciones.perfil || this._perfil;
         this._portada = opciones.portada || this._portada;
 
         this.setAttribute('autor', opciones.autor || this._autor);
-        this.setAttribute('title', opciones.title || this._title);
+        this.setAttribute('titulo', opciones.titulo || this._titulo);
         this.setAttribute('perfil', opciones.perfil || this._perfil);
         this.setAttribute('portada', opciones.portada || this._portada);
         this._datos = opciones;
@@ -60,40 +61,135 @@ class ArticlePreview extends HTMLElement {
     render() {
         this.shadowRoot.innerHTML = /* html */`
         <style>
+            :host {
+                /* Colores */
+                --Very-Dark-Grayish-Blue: hsl(217, 19%, 35%);
+                --Desaturated-Dark-Blue: hsl(214, 17%, 51%);
+                --Grayish-Blue: hsl(212, 23%, 69%);
+                --Light-Grayish-Blue: hsl(210, 46%, 95%);
+                
+                /* Tipografía */
+                --Manrope: 'Manrope', sans-serif;
+                --Font-weight-500: 500;
+                --Font-weight-700: 700;
+            }
+            *,
+            *::before,
+            *::after {
+                box-sizing: border-box;
+                margin: 0;
+                padding: 0;
+                border: 0;
+                font-size: 100%;
+                vertical-align: baseline;
+            }
             .article-preview {
-            border: 1px solid #ccc;
-            padding: 16px;
-            margin: 16px;
-            border-radius: 8px;
+                display: grid;
+                grid-template-columns: max(295px) minmax(0, 428px);
+                grid-template-areas:
+                    "portada article-content"
+                    "portada author-info";
+
+                background-color: white;
+                gap: 1.75rem 0;
+                border-radius: 8px;
+                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);   
+            }
+            .portada {
+                grid-area: portada;
+            
+            }
+            .portada img {
+                inline-size: 100%;
+                block-size: 100%;
+                border-radius: 8px 0 0 8px;
+                display: block;
+                object-fit: cover;
+                object-position: left;
+            }
+            .article-content {
+                grid-area: article-content;
+                padding: 1.75rem 1.75rem 0 ;
             }
             .title {
-            font-size: 1.5em;
-            margin-bottom: 8px;
+            font-size: 1.25rem;
+            margin-bottom: 1.5rem;
             }
             .content {
-            font-size: 1em;
+            font-size: 0.875rem;
             color: #555;
+            }
+            .author-info {
+                grid-area: author-info;
+                display: flex;
+                align-items: center;
+                gap: 1rem;
+                padding: 0 1.75rem 1.75rem;
+            }
+            .perfil {
+                inline-size: 2.5rem;
+                block-size: 2.5rem;
+                border-radius: 50%;
+            }
+            .perfil img {
+                inline-size: 100%;
+                block-size: 100%;
+                border-radius: 50%;
+                display: block;
+            }
+            .author-text {
+                flex-grow: 1;
+            }
+            .autor {
+                font-weight: var(--Font-weight-700);
+                color: var(--Very-Dark-Grayish-Blue);
+                font-size: 0.813rem;
+            }
+            .fecha {
+                font-weight: var(--Font-weight-500);
+                color: var(--Grayish-Blue);
+                font-size: 0.813rem;
+            }
+            .share {
+                align-self: center;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: var(--Light-Grayish-Blue);
+                padding: 0.5rem;
+                border-radius: 50%;
+                inline-size: 1.75rem;
+                block-size: 1.75rem;
+                cursor: pointer;
+                transition: background-color 0.3s ease;
+            }
+            .share img {
+                inline-size: 100%;
+                block-size: 100%;
+                object-fit: cover;
             }
         </style>
         <article class="article-preview">
-            <figure>
+            <figure class="portada">
                 <img src="${this._portada}" alt="Article Image Portada" />
             </figure>
-            <section>
-                <h2 class="title">${this._title}</h2>
+            <section class="article-content">
+                <h2 class="title">${this._titulo}</h2>
                 <p class="content"><slot></slot></p>
             </section>
-            <footer>
-                <figure>
+            <footer class="author-info">
+                <figure class="perfil">
                     <img src="${this._perfil}" alt="Article Image Perfil" />
                 </figure>
-                <p>${this._autor}</p>
-                <p>${new Date().toLocaleDateString('es-ES', {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric'
-                })}</p>
-                <span>
+                <div class="author-text">
+                    <p class="autor" >${this._autor}</p>
+                    <p class="fecha" >${new Date().toLocaleDateString('es-ES', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                    })}</p>
+                </div>
+                <span class="share">
                     <img src="./images/icon-share.svg" alt="Share Icon" />
                 </span>
             </footer>
@@ -105,10 +201,9 @@ class ArticlePreview extends HTMLElement {
 customElements.define('article-preview', ArticlePreview);
 
 /* const preview = document.querySelector('#preview');
-preview.articleData = {
-    title: 'Example Title',
-    content: 'This is the content of the article.'
-}; */
+preview.datos = {
+    autor: 'Ivan'
+} */
 
 /* const preview = document.createElement('article-preview');
 const $app = document.querySelector('#app');
@@ -118,11 +213,13 @@ const $app = document.querySelector('#app');
 console.log(preview.datos);
 
 $app.appendChild(preview);
-preview.setAttribute('autor', 'Ivan');
+// preview.setAttribute('autor', 'Ivan');
 
 preview.datos = {
     autor: 'Michelle Appleton',
     portada: "./images/drawers.jpg",
-    title: "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
+    titulo: "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
     perfil: "./images/avatar-michelle.jpg"
-}; */
+};
+
+console.log(preview.titulo) */
